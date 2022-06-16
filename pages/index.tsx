35850b1c53627ecb3735fbe44b1b27acef94a610
hishamdean <hisham.dean@gmail.com>
2022-06-16 23:23:51 +0800

    add styling and change header title

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import MainContainer from "../components/layout/MainContainer";
 const Home: NextPage = () => {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header title="Veterinary Clinic Directory" />
+      <Header title="Veterinary Clinic Listings" />
       <MainContainer />
     </Layout>
   );
